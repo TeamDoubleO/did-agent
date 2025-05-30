abd81ebb2271a9281e83252d7732bf56832942e4
willjsw <sunwoo1137@gmail.com>
2025-05-30 17:27:34 +0900

    KW-536/refactor: test application 추가

---
+++ src/test/java/com/doubleo/didagent/DidAgentApplicationTests.java
@@ -0,0 +1,11 @@
+package com.doubleo.didagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidAgentApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
