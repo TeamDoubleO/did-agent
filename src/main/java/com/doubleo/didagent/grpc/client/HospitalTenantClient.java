7384eed7901985d090d47294d72a143485e89ee6
coffeesigma <ljb22060@gmail.com>
2025-06-05 15:57:33 +0900

    KW-604/feat: grpc 구현 및 util class 추가

---
+++ src/main/java/com/doubleo/didagent/grpc/client/HospitalTenantClient.java
@@ -0,0 +1,35 @@
+package com.doubleo.didagent.grpc.client;
+
+import com.doubleo.tenantservice.domain.tenant.grpc.HospitalTenantServiceGrpc;
+import com.doubleo.tenantservice.domain.tenant.grpc.TenantWalletToken;
+import com.doubleo.tenantservice.domain.tenant.grpc.UpdateTokensRequest;
+import com.doubleo.tenantservice.domain.tenant.grpc.UpdateTokensResponse;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Slf4j
+@Component
+public class HospitalTenantClient {
+
+    @GrpcClient("tenant-service")
+    private HospitalTenantServiceGrpc.HospitalTenantServiceBlockingStub blockingStub;
+
+    public UpdateTokensResponse updateTokens(Map<String, String> tokens) {
+        UpdateTokensRequest.Builder builder = UpdateTokensRequest.newBuilder();
+
+        for (Map.Entry<String, String> entry : tokens.entrySet()) {
+            TenantWalletToken token = TenantWalletToken.newBuilder()
+                    .setTenantId(entry.getKey())
+                    .setWalletToken(entry.getValue())
+                    .build();
+            builder.addTokens(token);
+        }
+
+        UpdateTokensRequest request = builder.build();
+
+        return blockingStub.updateTokensByTenantId(request);
+    }
+}
