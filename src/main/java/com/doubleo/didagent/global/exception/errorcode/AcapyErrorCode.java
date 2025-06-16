a0f711060da5cb53755bfae1e70d9555fde824e2
willjsw <sunwoo1137@gmail.com>
2025-06-16 14:42:29 +0900

    KW-659/fix: 오타 수정

--- src/main/java/com/doubleo/didagent/global/exception/errorcode/AcapyErrorCode.java
+++ src/main/java/com/doubleo/didagent/global/exception/errorcode/AcapyErrorCode.java
@@ -10,7 +10,7 @@ public enum AcapyErrorCode implements BaseErrorCode {
     INVITATION_NOT_FOUND(HttpStatus.NOT_FOUND, "invitation 을 찾을 수 없습니다."),
     MEMBER_CONNECTION_NOT_FOUND(HttpStatus.NOT_FOUND, "member connection 을 찾을 수 없습니다."),
     INVITATION_REQUEST_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "invitation 생성 요청에 실패샜습니다"),
-    DID_PROCESS_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "DID 생성 요청에 실패샜습니다"),
+    DID_PROCESS_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "DID 생성 요청에 실패했습니다"),
     ;
 
     private final HttpStatus httpStatus;
