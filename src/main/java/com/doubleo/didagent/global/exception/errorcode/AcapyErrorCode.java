41bb1b9172878d3f97b4aad19e8cb393cb80fb25
willjsw <sunwoo1137@gmail.com>
2025-06-16 14:58:59 +0900

    KW-659/fix: INVITATION_REQUEST_FAILED 에러 메세지 오타 수정

--- src/main/java/com/doubleo/didagent/global/exception/errorcode/AcapyErrorCode.java
+++ src/main/java/com/doubleo/didagent/global/exception/errorcode/AcapyErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum AcapyErrorCode implements BaseErrorCode {
     INVITATION_NOT_FOUND(HttpStatus.NOT_FOUND, "invitation 을 찾을 수 없습니다."),
     MEMBER_CONNECTION_NOT_FOUND(HttpStatus.NOT_FOUND, "member connection 을 찾을 수 없습니다."),
-    INVITATION_REQUEST_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "invitation 생성 요청에 실패샜습니다"),
+    INVITATION_REQUEST_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "invitation 생성 요청에 실패했습니다"),
     DID_PROCESS_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "DID 생성 요청에 실패했습니다"),
     ;
 
